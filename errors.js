f171913133e5495852d36ebeda402563ebf225a4
Kristiyan Todorov <kristiyan.todorov@softwaregroup-bg.com>
2018-10-01 15:24:18 +0300

    fix: ut-error deprecations

--- errors.js
+++ errors.js
@@ -1,14 +1,14 @@
 'use strict';
-module.exports = defineError => {
-    const jsonRPC = defineError('jsonRPCCodec');
-    const invalidJson = defineError('invalidJson', jsonRPC, 'Invalid json');
+module.exports = ({defineError, getError, fetchErrors}) => {
+    if (!getError('jsonRPCCodec')) {
+        const jsonRPC = defineError('jsonRPCCodec', null, 'JSON RPC generic');
+        const invalidJson = defineError('invalidJson', jsonRPC, 'Invalid json');
 
-    return {
-        jsonRPC,
-        invalidJson,
-        invalidVersion: defineError('invalidVersion', invalidJson, 'Invalid jsonrpc version'),
-        invalidMethod: defineError('invalidMethod', invalidJson, 'Invalid or missing method'),
-        invalidPayload: defineError('invalidPayload', invalidJson, 'Invalid or missing jsonrpc payload'),
-        invalidMessageID: defineError('invalidMessageID', invalidJson, 'Invalid message id')
-    };
+        defineError('invalidVersion', invalidJson, 'Invalid jsonrpc version');
+        defineError('invalidMethod', invalidJson, 'Invalid or missing method');
+        defineError('invalidPayload', invalidJson, 'Invalid or missing jsonrpc payload');
+        defineError('invalidMessageID', invalidJson, 'Invalid message id');
+    }
+
+    return fetchErrors('jsonRPCCodec');
 };
